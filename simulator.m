45286f7a898ece23c881f3e5f19f82f331bc82d3
Grendel Yang <stevenygd@gmail.com>
2015-11-14 12:18:53 -0500

    [add]

--- simulator.m
+++ simulator.m
@@ -1,18 +1,29 @@
 %
 % load the data
 %   The simulator data includes:
-%   1. V = nodes information        : |V| x 4   matrix
+%   1. V = nodes information        : |V|  x 4   matrix
 %       V_{i,1}             is identity, 
 %       (V_{i,2},V_{i,3})   is the geographic point 
 %       V_{i,4}             is the population
 %
-%   2. EC = edge capacity           : |V| x |V| matrix
+%   2. EC = edge capacity           : |V|  x |V| matrix
 %       EC_{i,j}            capacity of the edge from V_i to V_j
 %
-%   3. W  = Wind information        : 1 x       matrix
+%   3. W  = Wind information        : 1    x 3   matrix
 %       (W_{1}, W_{2})      is the geographic point for landfall
 %       W_{3}               category
 %
+%   4. P  = Plan information        : n|V| x |V| matrix
+%       each |V|x|V| matrix denotes a plan day for a period
+%       plan for the first n period;
+%       if n > [numPeriod] then ignore all the next
+%
 
-V = loadfile();
+numPeriod = 5;
+pwd
+V  = importdata(strcat(pwd, '/input/nodes.dat'));
+EC = importdata(strcat(pwd, '/input/edgeCapacity.dat'));
+W  = importdata(strcat(pwd, '/input/wind.dat'));
+P  = importdata(strcat(pwd, '/input/wind.dat'));
+
 
